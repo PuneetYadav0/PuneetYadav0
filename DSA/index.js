bd290ffa5c558f9ea3054cfd771fc0f0b6563e92
PuneetYadav0 <Exampreparation9580@gmail.com>
2024-01-04 22:42:49 +0530

    Today Learnings

---
+++ DSA/index.js
@@ -0,0 +1,18 @@
+// const obj = {
+//     name : "Puneet",
+//     age : 19,
+//     city : "Lucknow"
+// }
+
+// console.log((Object.))
+
+// class Node{
+//     constructor(val) {
+//         this.val = val;
+//         this.next = null;
+//     }
+// }
+// let first  = new Node("Puneet");
+// first.next = new Node ("there");
+// first.next.next = new Node("Hello")
+// console.log(first.next.next);
